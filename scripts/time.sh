b9f338ed8cfbe626be047c86c091f698d4e12048
Andrés Sánchez <andres.sanchez@imdea.org>
2018-12-20 11:22:04 +0100

    When timing, each program can take up to mid-hour

--- scripts/time.sh
+++ scripts/time.sh
@@ -7,7 +7,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 for i in $(seq 1 $iterations); do
     echo "Iteration $i"
     rm -rf ../results/out/*
-    ./../check_security.sh > /dev/null 2> /dev/null
+    ./../check_security.sh -t 3600 > /dev/null 2> /dev/null
     file=../results/time/$(date "+%Y.%m.%d-%H.%M.%S")
     touch $file
     for x in ../results/out/*; do
