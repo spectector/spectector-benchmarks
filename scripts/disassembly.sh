09c2509bff227abfab9a5bd62d36360460e7c598
Andrés Sánchez <andres.sanchez@imdea.org>
2019-02-05 14:43:21 +0100

    First approximation to the disassembly script
    There are 3 steps:
          - Parse instructions
          - Parse data and bss sections
          - Parse references table and insert the references on assembly
          	code

---
+++ scripts/disassembly.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# Disassembly of binaries to at&t format
+
+objdump -d --disassembler-options=att --no-show-raw-insn $1 | tail -n+7 | sed "s/.*<\(.*\)>:/\1:/; s/\t/\n\t/" | sed "s/<.*//" | sed '0~2 s/[ ]\+//' > $1.s
+
+echo ".data" >> $1.s
+objdump -D -j .data $1 | sed 's/.*<\(.*\)>:/.type\t\1/'| sed 's/.*:\t\(.*\)\t.*/\1/' | sed 's/\([0-f][0-f]\) /.byte \1\n/g; s/\.\.\.//' | tail -n+8 >> $1.s
+
+echo ".bss" >> $1.s
+objdump -D -j .bss $1 | sed 's/.*<\(.*\)>:/.type\t\1/'| sed 's/.*:\t\(.*\)\t.*/\1/' | sed 's/\([0-f][0-f]\) /.byte \1\n/g; s/\.\.\.//' | tail -n+8 >> $1.s
+
+objdump -r --disassembler-options=att $1 | tail -n+6 | sed -n '/^$/q;p' | sed 's/.* .* / /; s/-.*//' > /tmp/table
+while read LINE; do
+    sed "0,/0x0(%rip)/{s/0x0(%rip)/$LINE(%rip)/}" $1.s > /tmp/out.s
+    cp /tmp/out.s $1.s
+done < /tmp/table
